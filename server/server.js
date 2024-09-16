074b8ff597bf9e33b68856db01782dbad4128252
datnguyen195 <thanhdat99201@gmail.com>
2024-09-16 23:03:33 +0700

    change

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const socket = require("./middlewares/socketio");
 const app = express();
 app.use(
   cors({
-    origin: "https://techshop-web-datn.onrender.com",
+    origin: "http://techshop-web-datn.onrender.com",
     methods: ["POST", "GET", "PUT", "DELETE"],
   })
 );
