3f8e9681c303dbf29232603f1a293d5657a80ee7
datnguyen195 <thanhdat99201@gmail.com>
2024-09-18 21:26:08 +0700

    noti

--- server/models/notification.js
+++ server/models/notification.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema({
-  userId: String,
+  title: String,
   message: String,
-  timestamp: { type: Date, default: Date.now },
+  imageUrl: String,
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Notification", notificationSchema);
--- server/routes/notification.js
+++ server/routes/notification.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const socket = require("../middlewares/socketio");
+const Notification = require("../models/notification");
 
-router.post("/send-notification", (req, res) => {
-  const { title, message } = req.body;
-  const notification = { title, message };
+router.post("/send-notification", async (req, res) => {
+  const { title, message, imageUrl } = req.body;
+  const notification = { title, message, imageUrl };
 
-  socket.sendNotification(notification);
-  res.status(200).json({ success: true, message: "Notification sent" });
+  try {
+    // Send notification via socket
+    socket.sendNotification(notification);
+
+    // Save the notification in the database
+    const newNotification = new Notification(notification);
+    await newNotification.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: "Notification sent and saved" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Error sending notification", error });
+  }
+});
+
+router.get("/notifications", async (req, res) => {
+  try {
+    // Lấy danh sách thông báo từ cơ sở dữ liệu, sắp xếp theo thời gian tạo mới nhất
+    const notifications = await Notification.find().sort({ createdAt: -1 });
+    res.status(200).json({ success: true, notifications });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error retrieving notifications",
+        error,
+      });
+  }
 });
 
 module.exports = router;
